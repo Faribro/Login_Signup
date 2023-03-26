885172a9d61d768ecb6d56bc8fa9764c85595c43
Farid Sayyed <faridsayyed1010@gmail.com>
2023-03-26 07:28:17 +0000

    Update 3 files

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,9 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../context/UserAuthContext";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet-control-geocoder/dist/Control.Geocoder.css";
-import "leaflet-control-geocoder/dist/Control.Geocoder.js";
-import L from "leaflet";
-import LeafletGeocoder from "./LeafletGeocoder";
-import LeafletRoutingMachine from "./LeafletRoutingMachine";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const position = [19.24033, 73.130539];
   const { logOut, user } = useUserAuth();
   const navigate = useNavigate();
   const handleLogout = async () => {
@@ -33,14 +27,11 @@ const Home = () => {
           Log out
         </Button>
       </div>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/*  <LeafletGeocoder /> */}
-        <LeafletRoutingMachine />
-      </MapContainer>
+       <div>
+      <Link to="https://leaflet-self.vercel.app/">
+        <button>Take Me To The Maps</button>
+      </Link>
+    </div>
     </>
   );
 };
